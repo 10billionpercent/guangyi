281e856470485cd23bf86d58cb5555ef84844e69
Shreya <shreya501711@gmail.com>
2026-05-31 15:16:23 +0530

    set up vite plugin pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["logo.png"],
+      manifest: {
+        name: "Guangyi",
+        short_name: "Guangyi",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "logo.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
